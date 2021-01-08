f86c1a98873b93059590cafe814a4e94dd122f2d
Lisa Lemons <me@lisa-lemons.com>
2021-01-08 12:33:08 -0500

    Generated type updates with latest changes to the API Listings

--- src/lib/graphql/queries/index.ts
+++ src/lib/graphql/queries/index.ts
@@ -1,2 +1,3 @@
+export * from './Listing';
 export * from './User';
 export * from './AuthUrl';
--- src/sections/Listing/index.tsx
+++ src/sections/Listing/index.tsx
@@ -1,5 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useQuery } from '@apollo/client';
+import { LISTING } from '../../lib/graphql/queries';
+import {
+  Listing as ListingData,
+  ListingVariables,
+} from '../../lib/graphql/queries/Listing/__generated__/Listing';
+import { RouteComponentProps } from 'react-router-dom';
+import { ErrorBanner, PageSkeleton } from '../../lib/components';
+import { Layout } from 'antd';
 
-export const Listing = () => {
+interface MatchParams {
+  id: string;
+}
+
+const { Content } = Layout;
+
+const PAGE_LIMIT = 3;
+
+export const Listing = ({ match }: RouteComponentProps<MatchParams>) => {
+  const [bookingsPage, setBookingsPage] = useState(1);
+
+  const { loading, data, error } = useQuery<ListingData, ListingVariables>(
+    LISTING,
+    {
+      variables: {
+        id: match.params.id,
+        bookingsPage,
+        limit: PAGE_LIMIT,
+      },
+    }
+  );
+
+  if (loading) {
+    return (
+      <Content className="listings">
+        <PageSkeleton />
+      </Content>
+    );
+  }
+
+  if (error) {
+    return (
+      <Content className="listings">
+        <ErrorBanner description="This listing may not exist or we've enountered an error. Please try again." />
+        <PageSkeleton />
+      </Content>
+    );
+  }
+
+  const listing = data ? data.listing : null;
+  const listingBookings = listing ? listing.bookings : null;
+
   return <h2>Listing</h2>;
 };
