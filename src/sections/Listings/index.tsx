c468e7a3bc55548cecccfa902cbf052c2785b79c
Lisa Lemons <me@lisa-lemons.com>
2021-03-27 16:36:56 -0400

    Added loading and  error handling UIfor listings

--- src/sections/Listings/index.tsx
+++ src/sections/Listings/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Layout, List, Typography } from 'antd';
+import React, { useState } from 'react';
+import { Affix, Layout, List, Typography } from 'antd';
 import { ListingsFilter } from '../../lib/graphql/globalTypes';
 import { useQuery } from '@apollo/client';
 import { LISTINGS } from '../../lib/graphql/queries';
@@ -7,8 +7,13 @@ import {
   Listings as ListingsData,
   ListingsVariables,
 } from '../../lib/graphql/queries/Listings/__generated__/Listings';
-import { ListingCard } from '../../lib/components';
+import { ErrorBanner, ListingCard } from '../../lib/components';
 import { Link, RouteComponentProps } from 'react-router-dom';
+import {
+  ListingsFilters,
+  ListingsPagination,
+  ListingsSkeleton,
+} from './components';
 
 const { Content } = Layout;
 const PAGE_LIMIT = 8;
@@ -17,34 +22,76 @@ interface MatchParams {
   location: string;
 }
 export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
-  const { data } = useQuery<ListingsData, ListingsVariables>(LISTINGS, {
-    variables: {
-      location: match.params.location,
-      filter: ListingsFilter.PRICE_LOW_TO_HIGH,
-      limit: PAGE_LIMIT,
-      page: 1,
+  const [filter, setFilter] = useState(ListingsFilter.PRICE_LOW_TO_HIGH);
+  const [page, setPage] = useState(1);
+
+  const { loading, data, error } = useQuery<ListingsData, ListingsVariables>(
+    LISTINGS,
+    {
+      variables: {
+        location: match.params.location,
+        filter,
+        limit: PAGE_LIMIT,
+        page,
+      },
     },
-  });
+  );
+
+  if (loading) {
+    return (
+      <Content className="listings">
+        <ListingsSkeleton></ListingsSkeleton>
+      </Content>
+    );
+  }
+
+  if (error) {
+    return (
+      <Content className="listings">
+        <ErrorBanner
+          description={`
+            We either couldn't find anything matching your search or have encountered an error.
+            If you're searching for a unique location, try searching again with more common keywords.
+          `}
+        />
+        <ListingsSkeleton />
+      </Content>
+    );
+  }
 
   const listings = data ? data.listings : null;
   const listingsRegion = listings ? listings.region : null;
 
   const listingsSectionElement =
     listings && listings.result.length ? (
-      <List
-        grid={{
-          gutter: 8,
-          xs: 1,
-          sm: 2,
-          lg: 4,
-        }}
-        dataSource={listings.result}
-        renderItem={(listing) => (
-          <List.Item>
-            <ListingCard listing={listing} />
-          </List.Item>
-        )}
-      />
+      <div>
+        <Affix offsetTop={64}>
+          <ListingsPagination
+            total={listings.total}
+            page={page}
+            limit={PAGE_LIMIT}
+            setPage={setPage}
+          ></ListingsPagination>
+          <ListingsFilters
+            filter={filter}
+            setFilter={setFilter}
+          ></ListingsFilters>
+        </Affix>
+        <List
+          grid={{
+            gutter: 8,
+            xs: 1,
+            sm: 2,
+            lg: 4,
+          }}
+          dataSource={listings.result}
+          renderItem={(listing) => (
+            <List.Item>
+              <ListingCard listing={listing} />
+            </List.Item>
+          )}
+        />
+      </div>
     ) : (
       <div>
         <Paragraph>
